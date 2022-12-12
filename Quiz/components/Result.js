debeca6191c0436422710336c53fce20e9e5674a
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2022-12-12 11:29:29 +0100

    add FlatList and Refresh Control

--- Quiz/components/Result.js
+++ Quiz/components/Result.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
-import {StyleSheet} from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
-import {DataTable} from 'react-native-paper';
+import {StyleSheet, RefreshControl, View, Text} from 'react-native';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
+
+const wait = timeout => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
 
 function Result() {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   const results = [
     {
       nick: 'Marek',
@@ -18,7 +28,8 @@ function Result() {
       total: 24,
       type: 'teatr polski',
       date: '2022-11-23',
-    },{
+    },
+    {
       nick: 'Marek',
       score: 18,
       total: 20,
@@ -62,41 +73,56 @@ function Result() {
     },
   ];
 
-  const Item=(item,index)=>(
- <DataTable.Row>
-        <DataTable.Cell style={styles.tableRow2}>{item.nick}</DataTable.Cell>
-        <DataTable.Cell style={styles.tableRow2}>{item.score}/{item.total}</DataTable.Cell>
-        <DataTable.Cell style={styles.tableRow2}>{item.type}</DataTable.Cell>
-        <DataTable.Cell style={styles.tableRow2}>{item.date}</DataTable.Cell>
-      </DataTable.Row>
-      
-
+  const Item = item => (
+    <View style={{flexDirection: 'row'}}>
+      <Text style={styles.tableRow2}>{item.nick}</Text>
+      <Text style={styles.tableRow2}>
+        {item.score}/{item.total}
+      </Text>
+      <Text style={styles.tableRow2}>{item.type}</Text>
+      <Text style={styles.tableRow2}>{item.date}</Text>
+    </View>
   );
 
-  const renderItem = ({ item }) => (
-    <Item nick={item.nick}
-    score={item.score}
-    total={item.total}
-    type={item.type}
-    date={item.date}
+  const renderItem = ({item}) => (
+    <Item
+      nick={item.nick}
+      score={item.score}
+      total={item.total}
+      type={item.type}
+      date={item.date}
     />
   );
 
   return (
-    <DataTable style={styles.container}>
-      <DataTable.Header style={styles.tableHeader}>
-        <DataTable.Title style={styles.tableTitle}>Nick</DataTable.Title>
-        <DataTable.Title style={styles.tableTitle}>point</DataTable.Title>
-        <DataTable.Title style={styles.tableTitle}>Type</DataTable.Title>
-        <DataTable.Title style={styles.tableTitle}>Date</DataTable.Title>
-      </DataTable.Header>
-     <FlatList
-     data={results}
-     renderItem={renderItem}
-     
-     />
-     
-    </DataTable>
+    <View>
+      <View
+        style={{
+          marginLeft: 20,
+          marginRight: 20,
+          padding: 10,
+          marginBottom: 100,
+        }}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text style={styles.tableTitle}>Nick</Text>
+          <Text style={styles.tableTitle}>Point</Text>
+          <Text style={styles.tableTitle}>Type</Text>
+          <Text style={styles.tableTitle}>Date</Text>
+        </View>
+        {/* <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }> */}
+        <FlatList
+          data={results}
+          renderItem={renderItem}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        />
+        {/* </ScrollView> */}
+      </View>
+    </View>
   );
 }
 
@@ -110,15 +136,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     borderWidth: 1,
+    textAlign: 'center',
+    fontSize: 25,
     backgroundColor: '#DCDCDC',
   },
   tableRow: {
+    flexDirection: 'row',
+    display: 'flex',
     backgroundColor: '#f2ffff',
     borderWidth: 1,
   },
   tableRow2: {
+    flex: 1,
+    fontSize: 20,
     backgroundColor: '#fff2ff',
     borderWidth: 1,
+    color: 'black',
+    textAlign: 'center',
   },
 });
 
