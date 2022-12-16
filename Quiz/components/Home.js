6d6f9c4ef4151678bf9306f4994a2e01cbfde5da
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2022-12-16 18:47:34 +0100

    get tests info

--- Quiz/components/Home.js
+++ Quiz/components/Home.js
@@ -1,84 +1,65 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import {View, Text, ActivityIndicator} from 'react-native';
+import {TouchableOpacity, FlatList} from 'react-native-gesture-handler';
 
 import HomeElement from './elements/HomeElement';
 
 function Home({navigation}) {
-    return (
-        <>
-        <ScrollView>
-      <View style={{ flex: 1,margin:30 }}>
-        
-        
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
-        <HomeElement
-        backgroundColor={"#ffffff"}
-        title={"QUIZ"}
-        text={"opis Quizuuuuu"}
-        colorText={"#000000"}
-        />
+  const [isLoading, setLoading] = React.useState(true);
+  const [tests, setTests] = React.useState([]);
 
-        
-        
-        
-      </View>
-      </ScrollView>
-      <View >
-        <TouchableOpacity style={{borderWidth:5,borderColor:'yellow',margin:15,marginTop:30,backgroundColor:"#2f2f2f"}} 
-        onPress={()=>navigation.navigate('Result')}>
-        <Text style={{fontSize:35,textAlign:'center',color:"#ffffff"}}>Wyniki</Text>
+  const getTests = async () => {
+    try {
+      const response = await fetch('https://tgryl.pl/quiz/tests');
+      const json = await response.json();
+      setTests(json);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    getTests();
+  }, []);
+
+  const renderItem = ({item}) => (
+    <HomeElement
+      backgroundColor={'#ffffff'}
+      title={item.name}
+      text={item.description}
+      lvl={item.level}
+      colorText={'#000000'}
+    />
+  );
+
+  return (
+    <>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View style={{flex: 1, margin: 30}}>
+          <FlatList data={tests} renderItem={renderItem} />
+        </View>
+      )}
+      <View>
+        <TouchableOpacity
+          style={{
+            borderWidth: 5,
+            borderColor: 'yellow',
+            margin: 15,
+            marginTop: 30,
+            backgroundColor: '#2f2f2f',
+          }}
+          onPress={() => navigation.navigate('Result')}>
+          <Text style={{fontSize: 35, textAlign: 'center', color: '#ffffff'}}>
+            Wyniki
+          </Text>
         </TouchableOpacity>
       </View>
-      </>
-    );
-  }
+    </>
+  );
+}
 
-  export default Home;
+export default Home;
