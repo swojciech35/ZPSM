e8bce98bbc6beff664935f6d0a63d09b583c8f34
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-01-12 11:42:49 +0100

    add tab navigation/ add Devices and Connection components

---
+++ IOT/components/Connection.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+function Connection() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Connection screen</Text>
+    </View>
+  );
+}
+
+export default Connection;
